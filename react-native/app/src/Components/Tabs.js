bbe5713ce99c44f2bcbeafc2e1eab93171283718
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-08-31 11:41:06 -0300

    scene/profile

--- react-native/app/src/Components/Tabs.js
+++ react-native/app/src/Components/Tabs.js
@@ -6,27 +6,31 @@ import Profile from './Profile'
 import Orders from './Orders'
 
 
-const renderScene = SceneMap({
+const renderScene = SceneMap({ // Declaramos las escenas/componentes que mostraremos en cada ruta de navegación.
     first: Profile,
     second: Orders,
 })
 
 export default function Tabs () {
 
-    const layout = useWindowDimensions()
+    // const layout = useWindowDimensions()
+    // initialLayout={{width: layout.width}}
+
     const [index, setIndex] = useState(0)
-    const [routes] = useState([
+    const [routes] = useState([ // seteamos las rutas de nuestra navegación con los títulos. Las keys tienen que coincidir con las escenas que queremos mostrar.
         { key: 'first', title:'PROFILE'},
         { key: 'second', title:'ORDERS'},
     ]
     )
 
+    // Para personalizar las barras de navegación
     const renderTabsBar = (props) => (
-        <TabBar {...props} tabStyle={styles.tabStyle} indicatorStyle={{backgroundcolor:'black'}} activeColor='#ffff00' inactiveColor='white' renderLabel={({route, color}) => (<Text style={{color, ...styles.text}}>{route.title}</Text>)}/>
+        <TabBar {...props}  tabStyle={styles.tabStyle} activeColor='#ffff00' inactiveColor='white' renderLabel={({route, color}) => (<Text style={{color, ...styles.text}}>{route.title}</Text>)}/>
     )
 
+    // Renderizamos la escena (renderScene), seteamos index para que cambiar entre barras de navegacion (onIndexChange, obligatorio)
     return (
-       <TabView navigationState={{index, routes}} renderScene={renderScene} onIndexChange={setIndex} initialLayout={{width: layout.width}} renderTabBar={renderTabsBar} />
+       <TabView navigationState={{index, routes}} renderScene={renderScene} onIndexChange={setIndex}  renderTabBar={renderTabsBar} />
     )
 }
 
