8d8c1614fbed6d2cc6ba4942166a42274b2bba53
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-08-31 19:20:06 -0300

    screen/payment

--- react-native/app/App.js
+++ react-native/app/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {  NativeBaseProvider } from 'native-base'
 import LoginScreen from './src/Screens/LoginScreen.js'
-import ShippingScreen from './src/Screens/ShippingScreen'
+import PaymentScreen from './src/Screens/PaymentScreen'
 
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <ShippingScreen/>
+      <PaymentScreen/>
     </NativeBaseProvider>
   );
 }
