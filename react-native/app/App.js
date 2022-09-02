534a49f08ef8ae0710bd0236e84f8673858a9b20
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-09-02 14:29:57 -0300

    feature/bottomNavigation

--- react-native/app/App.js
+++ react-native/app/App.js
@@ -1,22 +1,29 @@
 import React from 'react'
-import {  NativeBaseProvider } from 'native-base'
-import LoginScreen from './src/Screens/LoginScreen.js'
-import CartScreen from './src/Screens/CartScreen.js'
-import HomeScreen from './src/Screens/HomeScreen.js'
-import NotVerifyScreen from './src/Screens/NotVerifyScreen.js'
-import OrderScreen from './src/Screens/OrderScreen.js'
-import PaymentScreen from './src/Screens/PaymentScreen'
-import ProfileScreen from './src/Screens/ProfileScreen.js'
-import RegisterScreen from './src/Screens/RegisterScreen.js'
-import ShippingScreen from './src/Screens/ShippingScreen.js'
-import SingleProductScreen from './src/Screens/SingleProductScreen.js'
-import PlaceOrderScreen from './src/Screens/PlaceOrderScreen'
+import {  NativeBaseProvider, StatusBar } from 'native-base'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './src/Screens/LoginScreen'
+import RegisterScreen from './src/Screens/RegisterScreen'
+import OrderScreen from './src/Screens/LoginScreen'
+import BottomNav from './src/Navigations/BottomNav'
 
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <OrderScreen/>
+      <NavigationContainer>
+        <StatusBar hidden={true}/>
+        <Stack.Navigator initialRouteName='Bottom' screenOptions={{
+          headerShown: false
+        }}>
+          <Stack.Screen name='Login' component={LoginScreen}/>
+          <Stack.Screen name='Register' component={RegisterScreen}/>
+          <Stack.Screen name='Order' component={OrderScreen}/>
+          <Stack.Screen name='Bottom' component={BottomNav}/>
+
+        </Stack.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
---
+++ react-native/app/src/Navigations/BottomNav.js
@@ -0,0 +1,54 @@
+import { AntDesign, Entypo, FontAwesome, FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Center, Text } from 'native-base'
+import { Pressable, StyleSheet } from 'react-native'
+import React from 'react'
+import HomeScreen from '../Screens/HomeScreen'
+import ProfileScreen from '../Screens/ProfileScreen'
+import CartScreen from '../Screens/CartScreen'
+
+const Tab = createBottomTabNavigator()
+const CustomTab = ({children, onPress}) => (
+    <Pressable onPress={onPress} h={100} w={100} rounded='full' bg='#ff000f' top={-20} shadow={2}>
+        {children}
+    </Pressable>
+)
+
+export default function BottomNav () {
+    return (
+        <Tab.Navigator backBehavior='main' initialRouteName='Main' screenOptions={{tabBarShowLabel:false, tabBartStyle:{...styles.tab}, headerShown:false, tabBarHideOnKeyboard: true}}>
+            <Tab.Screen name='Main' component={HomeScreen} options={{tabBarIcon: ({focused}) => (
+            <Center> 
+                {focused 
+                ? <Entypo name='home' size={24} color='#ff000f'/>
+            : <AntDesign name='home' size={24} color='black'/>}
+            </Center>)}}
+            />
+
+            <Tab.Screen name='Cart' component={CartScreen} options={{tabBarIcon: ({focused}) => (
+            <Center bg='#ff000f' top={-20} h={60} w={60} rounded='full'> 
+                {focused 
+                ? <FontAwesome5 name='shopping-basket' size={24} color='white'/>
+            : <MaterialCommunityIcons name='shopping-outline' size={24} color='white'/>}
+            </Center>)}}
+            />
+
+            <Tab.Screen name='Profile' component={ProfileScreen} options={{tabBarIcon: ({focused}) => (
+            <Center> 
+                {focused 
+                ? <FontAwesome name='user' size={24} color='#ff000f'/>
+            : <AntDesign name='user' size={24} color='black'/>}
+            </Center>)}}
+            />
+
+        </Tab.Navigator>
+    )
+}
+
+const styles= StyleSheet.create({
+    tab: {
+        elevation: 0,
+        backgroundColor: 'white',
+        height: 60
+    }
+})
