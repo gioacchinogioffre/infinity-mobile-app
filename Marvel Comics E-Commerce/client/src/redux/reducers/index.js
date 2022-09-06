e3aa3cc63822960088aaaaa2a914f4eadf038f28
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-09-06 00:05:09 -0300

    feature/action/updateUser

--- Marvel Comics E-Commerce/client/src/redux/reducers/index.js
+++ Marvel Comics E-Commerce/client/src/redux/reducers/index.js
@@ -4,7 +4,8 @@ import {
    POST_USER,
    GET_USER,
    GET_COMICS,
-   GET_REVIEWS
+   GET_REVIEWS,
+   UPDATE_USER
 } from "../actions"
 
 
@@ -19,13 +20,18 @@ export default function rootReducer(state = initialState, action) {
 
     switch(action.type) {
 
+        //USER
+            case POST_USER: return {...state}
+
+            case GET_USER: return {...state, user: action.payload}
+
+            case UPDATE_USER: return {...state, user: action.payload}
+
+
         case GET_COMICS: return {...state, comics: action.payload }
 
         case GET_REVIEWS: return {...state, reviews: action.payload }
         
-        case POST_USER: return {...state}
-
-        case GET_USER: return {...state, user: action.payload}
 
         // case GET_SEARCH_COMICS: return { ...state, allProductsFiltered: action.payload }
 
--- Marvel Comics E-Commerce/client/src/screens/LoginScreen.js
+++ Marvel Comics E-Commerce/client/src/screens/LoginScreen.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, Box, Heading, VStack, Input, Button, Pressable} from 'native-base'
+import { useDispatch } from 'react-redux'
+import { Text, Box, Heading, VStack, Input, Button, Pressable} from 'native-base'
 import { MaterialIcons, FontAwesome} from '@expo/vector-icons';
 import * as Google from 'expo-auth-session/providers/google'
 import * as WebBroser from 'expo-web-browser'
+import { getUser } from '../redux/actions'
+
 
 WebBroser.maybeCompleteAuthSession() // nos permite completar la autenticacion desde el navegador y nos devuelve los resultados acá, es un modal || IMPORTANTE, SINO NO FUNCIONA
 
 export default function LoginScreen ({navigation}) {
 
+    const dispatch = useDispatch()
     const [accessToken, setAccesToken] = useState(null)
     const [user, setUser] = useState(null)
     const [request, response, promptAsync] = Google.useAuthRequest({
@@ -32,23 +36,44 @@ export default function LoginScreen ({navigation}) {
    }
 
    const [input, setInput] = useState({
-    name: '',
     email: '',
+    password: '',
    })
 
+   const login = () => {
+        dispatch(getUser(input.email, input))
+        navigation.navigate('Bottom')
+   }
+
+   const handleOnChange = ({name, value}) => {
+
+    setInput({
+        ...input,
+       [name]: value
+    })
+    // setError(validateForm({
+    //     ...input,
+    //     [e.target.name]:e.target.value
+    // }))
+}
 
     return (
         <Box flex={1} justifyContent='center' >
             <Heading>LOGIN</Heading>
             <VStack space={6} pt='4'>
-                <Input InputLeftElement={<MaterialIcons name="email" size={20} color="red" />} variant='underlined' placeholder='user@gmail.com' w='50%'></Input>
-                <Input type='password' InputLeftElement={<FontAwesome name="bullseye" size={20} color="red" />} variant='underlined' placeholder='********' w='50%'></Input>
+                <Input onChangeText={(value) => handleOnChange({name: 'email', value: value})}  InputLeftElement={<MaterialIcons name="email" size={20} color="red" />} variant='underlined' placeholder='user@gmail.com' w='50%'></Input>
+                <Input onChangeText={(value) => handleOnChange({name: 'password', value: value})}  type='text' InputLeftElement={<FontAwesome name="bullseye" size={20} color="red" />} variant='underlined' placeholder='********' w='50%'></Input>
             </VStack>
+            <Button 
+            onPress={login}
+             _pressed={{bg: '#000000'}} my={3} w= '40%' rounded={10} bg='#ff000f'>
+                LOGIN
+            </Button>
             <Button 
             // onPress={() => navigation.navigate('Bottom')}
             onPress={() => promptAsync({showInRecents: true})}
              _pressed={{bg: '#000000'}} my={3} w= '40%' rounded={10} bg='#ff000f'>
-                LOGIN
+                GOOGLE
             </Button>
             <Pressable onPress={() => navigation.navigate('Register')} mt={4}>
                 <Text>SIGN UP</Text>
