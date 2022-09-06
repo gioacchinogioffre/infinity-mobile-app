a216e7af2b15de04a4b9b5c1fe48d5e65932e2ef
Lauti0122 <93667112+Lauti0122@users.noreply.github.com>
2022-09-05 23:49:38 -0300

    fixed

--- Marvel Comics E-Commerce/api/src/controllers/user.controllers.js
+++ Marvel Comics E-Commerce/api/src/controllers/user.controllers.js
@@ -1,4 +1,6 @@
 import User from "../models/User"
+import jwt from 'jsonwebtoken';
+import config from '../config'
 
 export const getUser = async(req,res)=>{
    try{
@@ -25,13 +27,19 @@ export const updateUser = async (req,res)=>{
     
 }
 
-export const getUserByID = async (req,res)=>{
+export const getUserByEmail = async (req,res)=>{
     try{
-        const {UserID} = req.params
-        const user = await User.findById(UserID)
+        const {email} = req.params
+        const {password} = req.body
+        const user = await User.findOne({email: email})
         if(!user){
-            res.status(400).json({message:'user not found'})
+            return res.status(400).json({message:'User not found'})
         }
+
+        const matchPassword = await User.comparePassword(password, user.password)
+        
+        if(!matchPassword) return res.status(401).json({message:"Invalid Password"})
+
         res.status(200).json(user)
     }
     catch(error){
--- Marvel Comics E-Commerce/api/src/routes/user.js
+++ Marvel Comics E-Commerce/api/src/routes/user.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { getUser,updateUser, getUserByID } from "../controllers/user.controllers";
+import { getUser,updateUser, getUserByEmail } from "../controllers/user.controllers";
 import {authJWT, verifySignUP} from '../middlewares'
 const router = Router()
 // router.post('/',[authJWT.verifyToken, authJWT.isAdmin, verifySignUP.checkRolesExisted], createUser)
 router.get('/',getUser)
-router.get('/:UserID', getUserByID)
+router.get('/:email', getUserByEmail)
 router.put('/:userID',updateUser)
 export default router
