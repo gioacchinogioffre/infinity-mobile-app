d2819b9f02df3b00bdfeb72f794eb59217994463
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-09-13 11:00:39 -0300

    feature/customers-employees

--- Marvel Comics E-Commerce/dashboard/src/pages/Customers.jsx
+++ Marvel Comics E-Commerce/dashboard/src/pages/Customers.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Toolbar, Sort, Filter, Edit } from '@syncfusion/ej2-react-grids'
+
+import { customersData, customersGrid } from '../data/dummy'
+import { Header } from '../components'
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className='mt-12 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+    <Header category='Page' title='Customers'/>
+    <GridComponent dataSource={customersData} allowPaging allowSorting editSettings={{allowDeleting: true, allowEditing: true, allowAdding: true}} allowPdfExport toolbar={['Search', 'Delete', 'Add']} width='auto'  >
+      <ColumnsDirective>
+        {customersGrid.map((item,index) => (
+          <ColumnDirective key={index} {...item}/>
+        ))}
+      </ColumnsDirective>
+      <Inject services={[Page, Sort, Toolbar, Selection, Edit, Filter]}/>
+    </GridComponent>
+  </div>
   )
 }
 
--- Marvel Comics E-Commerce/dashboard/src/pages/Employees.jsx
+++ Marvel Comics E-Commerce/dashboard/src/pages/Employees.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Search, Page, ExcelExport, PdfExport, Edit, Inject, Sort, Toolbar, Selection } from '@syncfusion/ej2-react-grids'
+
+import { employeesData, employeesGrid } from '../data/dummy'
+import { Header } from '../components'
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className='mt-12 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+    <Header category='Page' title='Employees'/>
+    <GridComponent dataSource={employeesData} allowPaging allowSorting allowPdfExport editSettings={{allowDeleting: true, allowEditing: true, allowAdding: true}} toolbar={['Search', 'Delete', 'Add']}  width='auto'  >
+      <ColumnsDirective>
+        {employeesGrid.map((item,index) => (
+          <ColumnDirective key={index} {...item}/>
+        ))}
+      </ColumnsDirective>
+      <Inject services={[Page, Search, Sort, Toolbar, Edit, PdfExport, Selection]}/>
+    </GridComponent>
+  </div>
   )
 }
 
